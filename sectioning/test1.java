e0364dfa4be16767ef577a09e662221324a64b30
NouranMohamedFouad <nouranloly@gmail.com>
2023-05-17 03:24:04 +0300

    updated StudentScheduleTable class

---
+++ sectioning/test1.java
@@ -0,0 +1,13 @@
+package org.unitime.timetable.gwt.client.sectioning;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class test1 
+{
+	@Test
+    public void test1()
+	{
+		assertTrue(true);		
+	}
+}
